37a3d3c0b2aee19dd4ffeacc783deec58265bf92
al-jabir <aljabir098@gmail.com>
2022-10-03 18:54:17 +0600

    variable code

---
+++ Learn first code/variable.py
@@ -0,0 +1,29 @@
+a = 5
+b = 7
+c = 9
+d = 3
+
+print(a*b+c/d)
+print(a-b+c/d)
+print(a % b*c+d)
+print(a+b**c-d)
+
+code = 'python'
+
+print(code)
+
+first = 'I am still learning python'
+
+print(first)
+
+print(type(first))
+
+print(type(d))
+
+a = 5.69
+
+print(type(a))
+
+# aa = input()
+
+# print(aa)
