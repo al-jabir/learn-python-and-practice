9a52fdef8c4f21c8af1561339bf45093c55b80c9
al-jabir <aljabir098@gmail.com>
2022-10-03 19:04:23 +0600

    Operators

---
+++ Learn first code/operation.py
@@ -0,0 +1,33 @@
+a = 5
+b = 6
+
+c = 5
+d = 9
+
+print(a == c)
+
+print(a != d)
+
+print(a**d)
+
+print(a == b)
+
+
+print(a <= b)
+
+print(b >= c)
+
+print(c == d)
+
+
+a = 'bangladesh'
+b = 'desh'
+
+print(b in a)
+
+print(a in b)
+
+n = 'khan'
+n2 = 98
+
+print(n is not n2)
