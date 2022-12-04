5d838160bae98785b4e468e3f70afc7721fff3d7
al-jabir <aljabir098@gmail.com>
2022-12-04 12:11:03 +0600

    practice test python

--- Test/test.py
+++ Test/test.py
@@ -1,1 +1,29 @@
-print('king')
+print('kings')
+
+print('Hello Developers!')
+
+print("I will be developer");
+
+a = 'King'
+
+b = 'mafia'
+
+print(b)
+
+x = a+' ' +b
+print(x)
+
+a = 'Jack'
+
+print(a)
+
+n = 6
+s = 5
+
+print(n*s)
+
+print(n**s)
+
+print(n/s)
+
+print(n*s%2)
