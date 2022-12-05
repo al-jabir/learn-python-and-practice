c7a87ba0ac435936c67dcf24e4dfd8ab6e549e70
al-jabir <aljabir098@gmail.com>
2022-12-05 09:27:30 +0600

    I have some learn user input data

---
+++ Input/input.py
@@ -0,0 +1,14 @@
+user_input = input('What is your username?')
+
+print(user_input)
+
+nxt = 'My username is '+ user_input
+
+print(nxt)
+
+user_age = input('What is your age? ')
+user_dad_age = input('What is your dad age? ')
+
+total = user_age + user_dad_age
+
+print('Ours total: '+total)
---
+++ Input/input_math.py
@@ -0,0 +1,21 @@
+n = input('Please type an integer and press enter: ')
+n2 = input('please type another integer and press enter: ')
+
+num = int(n)
+num2 = int(n2)
+
+print('The number of ', num * num2)
+
+n3 = input('please type some number and press enter: ')
+
+n4 = input('please type another flot number and press enter: ')
+
+num3 = int(n3)
+num4 = float(n4)
+
+result = 'The number of '
+
+print(result, num3 * num4)
+print(result, num3 / num4)
+print(result, num3 + num4)
+print(result, num3 - num4)
