0b332d020331e728535a520dc67eb9d610edd4db
al-jabir <aljabir098@gmail.com>
2022-12-05 09:05:21 +0600

    datatype

---
+++ datatype/datatype.py
@@ -0,0 +1,20 @@
+aj = 'I love you'
+
+print(type(aj))
+
+a = False
+
+b = True
+
+c = a == b
+
+num = 1998
+
+num2 = 19.98
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(num))
+print(type(num2))
+
