b826491da9aa430017b95b21e8d9fef0bcb42a20
al-jabir <aljabir098@gmail.com>
2022-12-05 20:52:47 +0600

    while

---
+++ loop/while.py
@@ -0,0 +1,18 @@
+i = 0
+while i < 10:
+    print(i, ' I love coding...')
+    i += 1
+    
+j = 5
+while j >= 0:
+    j-= 1
+    print(j)
+
+n = input('Please enter positive number: ')
+n = int(n)
+
+m = 1
+while m <= 10:
+    print(n, 'x', m, '=' , n*m)
+    m+=1
+    
