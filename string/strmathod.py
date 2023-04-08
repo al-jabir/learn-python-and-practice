98b3d19ed4b7377d06e56266dd21ccca0531c514
al-jabir <aljabir098@gmail.com>
2023-04-08 20:26:28 +0600

    string mathod

---
+++ string/strmathod.py
@@ -0,0 +1,9 @@
+myP = "I will be good software developer."
+lstr = input("Please enter your type text: ")
+
+print(myP.upper())
+print(myP.capitalize())
+print(myP.find('software'))
+print(myP.replace('software', 'Backend'))
+
+print(f'{myP} my test is {len(lstr)} letters ')
