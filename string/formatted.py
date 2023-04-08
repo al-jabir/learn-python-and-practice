3c9fe9f671acf7d45a5a05de0ff2d8f53f4c317b
al-jabir <aljabir098@gmail.com>
2023-04-08 20:26:14 +0600

    string formatted

---
+++ string/formatted.py
@@ -0,0 +1,11 @@
+name = 'Johnny'
+name2 = 'Abdullah'
+age = 25
+job = 'Junior Software Developer'
+
+print(f'hi {name}. Your age is {age} years old right')
+
+print(f'I am {name2}. I am {age} years old. I will be {job} insha\'Allah.')
+
+print('I am {}. Your age is {} years old. I will be {}'.format('mafia','21','Backend developer'))
+print('I am {}. Your age is {} years old. I will be {}'.format(name,age,job))
