1e2dd9401cb87d345e8283864beafa93b37bde29
al-jabir <aljabir098@gmail.com>
2023-04-09 21:23:33 +0600

    list

---
+++ list/list.py
@@ -0,0 +1,26 @@
+fallList = ['Mango', 'PineApple', 'Pine', 'Orange', 'Juice']
+
+print(fallList)
+
+# list of len
+
+print(len(fallList))
+
+# something list of data stored
+
+myList = ['Mafia', 23, 'Married', True, 'Software Developer', 2023]
+
+print(myList)
+
+# myList of length data store
+
+print(len(myList))
+
+# check of mylist data store
+
+myList[4] = 'Software Engineer'
+
+print(myList[4])
+
+# again check of mylist data store
+print(myList)
