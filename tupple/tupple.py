77eca74bc91653ec94a5efe1ebda25bf5cdd48b8
al-jabir <aljabir098@gmail.com>
2023-04-10 15:56:41 +0600

    tupple

---
+++ tupple/tupple.py
@@ -0,0 +1,13 @@
+t = ('dal', 'bat', 'bath', 'bathroom', 'washroom', 'water')
+
+print(type(t))
+print(t)
+
+# check index of tupple data store
+
+print(t[3:5])
+print(t[5:])
+print(t[:5])
+
+myT = ('dal', 'bat', 'bathroom', 'bathroom')
+print(myT)
